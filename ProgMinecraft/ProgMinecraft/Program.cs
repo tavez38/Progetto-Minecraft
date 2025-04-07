6b33e6a93edb0ab6c38b739f1eac85f8738d53d4
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2025-04-07 15:52:51 +0200

    implementazione generazione inventario di avvio gioco

--- ProgMinecraft/ProgMinecraft/Program.cs
+++ ProgMinecraft/ProgMinecraft/Program.cs
@@ -1,16 +1,40 @@
-﻿namespace ProgMinecraft
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace ProgMinecraft
 {
     internal class Program
     {
         const int RAW_INVENTARY = 3;
         const int COLL_INVENTARY = 10;
+        const int MAT_AVVIO = 4;
         static String[] nomeMateriale = new String[COLL_INVENTARY];
         static int[] quantitaMateriale = new int[COLL_INVENTARY];
         static String[,] nomeMaterialeInventario = new String[RAW_INVENTARY, COLL_INVENTARY];
         static String[] possibiliMaterialiAvvio = { "TRONCO_LEGNO", "COBBLESTONE", "CARBONE", "CARNE_CRUDA", "PATATE", "CAROTE" };
+        static bool[] statusMatGen = { false, false, false, false, false, false };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random rand = new Random();
+            int matGen = rand.Next(0, 6);
+            int quantGen = rand.Next(1, 65);
+            int slotGen = 0;
+            while (slotGen < MAT_AVVIO) {
+                bool res = genSlotInventory(matGen, quantGen, slotGen);
+                if (res) {
+                    slotGen++;
+                }
+            }
+        }
+        static bool genSlotInventory(int posMatGen, int quantMatGen, int posInventory)
+        {
+            if (!statusMatGen[posMatGen]) 
+            { 
+                nomeMateriale[posInventory] = possibiliMaterialiAvvio[posMatGen];
+                quantitaMateriale[posInventory] = quantMatGen;
+                return true;
+            }
+            return false;
         }
     }
 }
