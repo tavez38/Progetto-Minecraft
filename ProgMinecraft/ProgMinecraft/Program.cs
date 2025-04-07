e48bf167a5ff4cc37bbe6d760491279d8b47d9d6
tavez38 <tavecchia.lorenzo.22@itisriva.edu.it>
2025-04-07 15:33:19 +0200

    dichiarazione strutture dati

---
+++ ProgMinecraft/ProgMinecraft/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ProgMinecraft
+{
+    internal class Program
+    {
+        const int RAW_INVENTARY = 3;
+        const int COLL_INVENTARY = 10;
+        static String[] nomeMateriale = new String[COLL_INVENTARY];
+        static int[] quantitaMateriale = new int[COLL_INVENTARY];
+        static String[,] nomeMaterialeInventario = new String[RAW_INVENTARY, COLL_INVENTARY];
+        static String[] possibiliMaterialiAvvio = { "TRONCO_LEGNO", "COBBLESTONE", "CARBONE", "CARNE_CRUDA", "PATATE", "CAROTE" };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
